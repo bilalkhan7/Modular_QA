ed24024b9034b93c85d534763462f3573df8f199
bilalkhan7 <50269802+bilalkhan7@users.noreply.github.com>
2020-03-22 22:07:03 +0100

    new menu intergration with service

--- Modular_QA/Project_Frontend/research-app/src/app/services/table.service.ts
+++ Modular_QA/Project_Frontend/research-app/src/app/services/table.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs' 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 @Injectable()
+
 export class TableService {
-
-  constructor(private http: Http) { }
-
-  fetchData(): Observable<Object> {
-    console.log('Test Table Service');
+ constructor(private http: HttpClient) { }
+  fetchData=(): Observable<Object>=> {
     return this.http.get('http://localhost:3000/table_attributes');
   } 
 }
