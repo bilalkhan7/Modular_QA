d8e857928df9b4b15fd16cac72b929075fe1cd7f
bilalkhan7 <50269802+bilalkhan7@users.noreply.github.com>
2020-03-23 01:51:29 +0100

    latest update drag and drop

--- Modular_QA/Project_Frontend/research-app/src/app/layouts/home/admin-layout.component.ts
+++ Modular_QA/Project_Frontend/research-app/src/app/layouts/home/admin-layout.component.ts
@@ -11,86 +11,9 @@ declare const $: any
   styleUrls: ['./admin-layout.component.scss'],
 })
 
-export class AdminLayoutComponent implements OnInit, AfterViewInit {
-    public navItems: NavItem[]
-
-    @ViewChild('sidebar') sidebar: any
-    @ViewChild(NavbarComponent) navbar: NavbarComponent
+export class AdminLayoutComponent  {
+    
     constructor() {
     }
-    ngOnInit() {
-        const elemMainPanel = <HTMLElement>document.querySelector('.main-panel')
-        const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper')
-        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-            let ps = new PerfectScrollbar(elemMainPanel)
-            ps = new PerfectScrollbar(elemSidebar)
-        }
-        this.navItems = [
-          { type: NavItemType.NavbarLeft, title: 'Dashboard', iconClass: 'fa fa-dashboard' },
-
-          {
-            type: NavItemType.NavbarRight,
-            title: '',
-            iconClass: 'fa fa-bell-o',
-            numNotifications: 5,
-            dropdownItems: [
-              { title: 'Notification 1' },
-              { title: 'Notification 2' },
-              { title: 'Notification 3' },
-              { title: 'Notification 4' },
-              { title: 'Another Notification' }
-            ]
-          },
-          {
-            type: NavItemType.NavbarRight,
-            title: '',
-            iconClass: 'fa fa-list',
-
-            dropdownItems: [
-              { iconClass: 'pe-7s-mail', title: 'Messages' },
-              { iconClass: 'pe-7s-help1', title: 'Help Center' },
-              { iconClass: 'pe-7s-tools', title: 'Settings' },
-               'separator',
-              { iconClass: 'pe-7s-lock', title: 'Lock Screen' },
-              { iconClass: 'pe-7s-close-circle', title: 'Log Out' }
-            ]
-          },
-          { type: NavItemType.NavbarLeft, title: 'Search', iconClass: 'fa fa-search' },
-
-          { type: NavItemType.NavbarLeft, title: 'Account' },
-          {
-            type: NavItemType.NavbarLeft,
-            title: 'Dropdown',
-            dropdownItems: [
-              { title: 'Action' },
-              { title: 'Another action' },
-              { title: 'Something' },
-              { title: 'Another action' },
-              { title: 'Something' },
-              'separator',
-              { title: 'Separated link' },
-            ]
-          },
-          { type: NavItemType.NavbarLeft, title: 'Log out' }
-        ]
-    }
-    ngAfterViewInit() {
-        this.runOnRouteChange()
-    }
-    runOnRouteChange(): void {
-      if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-        const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper')
-        const elemMainPanel = <HTMLElement>document.querySelector('.main-panel')
-        let ps = new PerfectScrollbar(elemMainPanel)
-        ps = new PerfectScrollbar(elemSidebar)
-        ps.update()
-      }
-    }
-    isMac(): boolean {
-        let bool = false
-        if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
-            bool = true
-        }
-        return bool
-    }
+    
 }
