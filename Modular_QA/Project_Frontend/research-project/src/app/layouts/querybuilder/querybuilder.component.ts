3bfe3b0f1edcdaee391b723400a3740d5ddf297d
Rizwan Mehmood <rizwanmehmoodstud@gmail.com>
2020-04-24 17:02:41 +0200

    operator list update and json post request

--- Modular_QA/Project_Frontend/research-project/src/app/layouts/querybuilder/querybuilder.component.ts
+++ Modular_QA/Project_Frontend/research-project/src/app/layouts/querybuilder/querybuilder.component.ts
@@ -11,31 +11,96 @@ import { TableService } from '../services/table.service'
   selector: 'app-querybuilder',
   templateUrl: './querybuilder.component.html',
   styleUrls: ['./querybuilder.component.css'],
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.Default,
+  providers: [TableService]
 
 
 })
 export class QuerybuilderComponent implements OnChanges, OnInit {
-  [x: string]: any;
 
   @Input() tableMapDrop: TablesMap[];
   @ViewChild(QueryBuilderComponent, { static: false }) queryBuilder: QueryBuilderComponent;
   public tables_name: Array<string> = [];
-  public currentConfig: QueryBuilderConfig;
+  public currentConfig: any = [];
   public allowRuleset = true;
   public allowCollapse: boolean;
-  public persistValueOnFieldChange = false;
+  public persistValueOnFieldChange = true;
   public queryCtrl: FormControl;
+
   @Input() config: QueryBuilderConfig;
   public query: { condition: string; rules: { field: any; operator: string; value: string; }[]; };
-  public entity_table: string='';
-   private characterType:string="character varying";
-   private NumericType:string="numeric";
-   private IntergerType:string="integer";
-   private DateType:string="date";
-   private TimeType:string="timestamp without time zone";
-
-  constructor(private formBuilder: FormBuilder) {
+  public entity_table: string = '';
+  private characterType: string = "character varying";
+  private NumericType: string = "numeric";
+  private IntergerType: string = "integer";
+  private DateType: string = "date";
+  private TimeType: string = "timestamp without time zone";
+  myOperatorMap = {
+    string: [
+      'equal',
+      'not_equal',
+      'begins_with',
+      'not_begins_with',
+      'contains',
+      'not_contains',
+      'ends_with',
+      'not_ends_with'
+    ],
+    number: [
+      'equal',
+      'not_equal',
+      'greater',
+      'greater_or_equal',
+      'between',
+      'less',
+      'less_or_equal',
+      'begins_with',
+      'not_begins_with',
+      'contains',
+      'not_contains',
+      'ends_with',
+      'not_ends_with'
+    ],
+    time: [
+      'equal',
+      'not_equal',
+      'greater',
+      'greater_or_equal',
+      'between',
+      'less',
+      'less_or_equal',
+      'begins_with',
+      'not_begins_with',
+      'contains',
+      'not_contains',
+      'ends_with',
+      'not_ends_with'
+    ],
+    date: [
+      'equal',
+      'not_equal',
+      'greater',
+      'greater_or_equal',
+      'between',
+      'less',
+      'less_or_equal'
+    ],
+    category: [
+      'equal',
+      'not_equal',
+      'in',
+      'not_in'
+    ],
+    boolean: [
+      'equal',
+      'not_equal'
+    ],
+    multiselect: [
+      'in',
+      'not_in'
+    ]
+  };
+  constructor(private formBuilder: FormBuilder,private webTableService: TableService) {
 
   }
 
@@ -51,141 +116,133 @@ export class QuerybuilderComponent implements OnChanges, OnInit {
   }
 
   userExpression: String = 'Attribute = undefined';
-  
+
   MapData() {
-   var mapArray = [];
-   var mapDataTypes=[];
-   var fieldColumn;
-   var fieldDataType;
-   let name: string;
-   var uiExpression = {};
-   var queryPost={};
-   var fieldsS = {}
-   name = `Plunker! v${VERSION.full}`;
-    this.tableMapDrop.map(item => this.entity_table=item.table_name.toString());
-  //  console.log('droptable'+this.tableMapDrop);
+    var mapArray = [];
+    var mapDataTypes = [];
+    var fieldColumn;
+    var fieldDataType;
+    let name: string;
+    let $uiExpression = {};
+
+    // var uiExpression = {};
+    var queryPost = {};
+    var fieldsS = {};
+    name = `Plunker! v${VERSION.full}`;
+
+
+
+    this.tableMapDrop.map(item => this.entity_table = item.table_name.toString());
     var columnArrayLenght = this.tableMapDrop.map(item => item.field.length);
-   // console.log('droptable'+columnArrayLenght);
+
+    //get attributes and map dataTypes
     for (let i = 0; i <= columnArrayLenght[0]; i++) {
       mapArray.push(this.tableMapDrop.map(item => item.field[i]))
       mapDataTypes.push(this.tableMapDrop.map(item => item.data_type[i]))
-  
+
     }
 
     fieldColumn = mapArray.map(items => items.toString())
-    fieldDataType = mapDataTypes.map(items => items.toString())  
-   // console.log("field"+fieldDataType);
-    this.config =
-    {
+    fieldDataType = mapDataTypes.map(items => items.toString())
 
-      fields: {
-              
+
+    for (var i = 0; i < fieldColumn.length - 1; i++) {
+
+      this.config =
+      {
+        
+
+        fields: {
+
+        }
       }
-    }
 
-    for (var i = 0; i < fieldColumn.length-1; i++) {
-
-       fieldsS[fieldColumn[i]] = {
+      fieldsS[fieldColumn[i]] = {
         name: fieldColumn[i],
         type: this.MapDataTypes(fieldDataType[i]),
-        operator:this.MapOperators(fieldDataType[i]),
-        entity:'currency',
+        //operator: this.MapOperators(fieldDataType[i]),
+        entity: this.entity_table,
         options: ''
       }
       this.config.fields = fieldsS;
-     
-     // this.detect.markForCheck();
+     // this.config.allowEmptyRulesets = true;
+      // this.detect.markForCheck();
       console.log('config ', JSON.stringify(this.config))
-      if (fieldColumn.length-1 > 0) {
-       // console.log('attributes length > 0');
+      if (fieldColumn.length - 1 > 0) {
+        // console.log('attributes length > 0');
 
-       uiExpression = {
-          condition: 'and',
-          rules: [
-            {
-              field: this.config.fields[fieldColumn[i]],
-            }
-          ]
+        $uiExpression = {
+            condition: 'and',
+            rules: [
+              {
+                field: this.config.fields[fieldColumn[i]],
+              
+              },
+              
+            ]
+          
         }
+
       }
+
     }
-      
-    
-    console.log(console.log('uiExpression ', JSON.stringify(this.userExpression)));
+
+    this.currentConfig[0] = this.config;
+
+    //  console.log('current Config'+this.currentConfig[i])
+    // console.log(console.log('uiExpression ', JSON.stringify($uiExpression)));
 
   }
 
 
-MapDataTypes(type: String)
-{
-var datatype='';
-//console.log('datatype',type);
+  passData($uiExpression) {
+    let querySend = [];
+
+    let objeTable = {[this.entity_table]: $uiExpression };
+    querySend[0] = objeTable;
+
+    this.webTableService.sendPostRequest(querySend).subscribe(
+      res => {
+        console.log(res);
+      }
+);
+    console.log(console.log('uiExpression ', JSON.stringify(querySend)));
+
+  }
 
 
-if(type ===this.characterType )
-{
-  datatype = 'string';
- 
-}
+  MapDataTypes(type: String) {
+    var datatype = '';
+    //console.log('datatype',type);
 
- else if(type === this.NumericType || type===this.IntergerType)
- {
-   datatype ='number'
-  
- }
- else if(type === this.DateType)
-{
-  //console.log('type',type);
-  datatype='date'
- ;
-}
-else if(type===this.TimeType)
-{
-datatype='date'
-}
-else
-{
-  datatype='string';
-  
-}
 
-return datatype;
-}
+    if (type === this.characterType) {
+      datatype = 'string';
 
-MapOperators(type:String)
-{
-  //type='';
-var operators=[];
-if(type ===this.characterType )
-{
-  operators =  ['=', 'like', '%'];
- }
+    }
 
- else if(type === this.NumericType || type===this.IntergerType)
- {
-  operators =  ['=', '<', '>'];
-  
- }
- else if(type === this.DateType)
-{
-  //console.log('type',type);
-  operators =  ['=', 'between'];
- ;
-}
-else if(type===this.TimeType)
-{
-  operators =  ['=', 'like', '%'];
-}
-else
-{
-  operators =  ['=', '<', 'between'];
-  
-}
+    else if (type === this.NumericType || type === this.IntergerType) {
+      datatype = 'number'
 
-return operators;
+    }
+    else if (type === this.DateType) {
+      //console.log('type',type);
+      datatype = 'date'
+        ;
+    }
+    else if (type === this.TimeType) {
+      datatype = 'date'
+    }
+    else {
+      datatype = 'string';
 
-}
- 
+    }
+
+    return datatype;
+  }
+
+
+
 
 
   private refreshField(field: string): void {
@@ -205,10 +262,31 @@ return operators;
     }
   }
 
-  findQueryInput(type: string): QueryInputDirective {
-    const templates = this.parentInputTemplates || this.inputTemplates;
-    return templates.find((item) => item.queryInputType === type);
-  }
+
+
+/*   setExpressionParam(): FieldMap{
   
+    let objList: Field[] = []; 
+      this.Map.parameters.forEach(param => { 
+            let x = { name: param.parameterName, type: 'string' }
+            if (objList.indexOf(x) === -1) {
+              objList.push(x); 
+            }  
+      }); 
+
+    const arrayToObject = (array) =>
+        array.reduce((obj, item) => {
+          obj[item.name] = item
+          return obj
+        }, {})
+    
+    const fieldMap:FieldMap = arrayToObject(objList)
+
+    console.log(objList);
+    console.log(fieldMap); 
+    return fieldMap; ;
   
+}  */
+
+
 }
--- Modular_QA/Project_Frontend/research-project/src/app/layouts/services/table.service.ts
+++ Modular_QA/Project_Frontend/research-project/src/app/layouts/services/table.service.ts
@@ -6,8 +6,19 @@ import {TablesMap} from '../interface/tables.map';
 
 @Injectable()
 export class TableService {
+  getdataurl="http://localhost:3000/table_attributes";
+ // postDataUrl="http://httpbin.org/post"
+  postDataUrl="http://localhost:3000/get_data"
+  postDataServer={
+    test:'test'
+  }
  constructor(private http: HttpClient) { }
   fetchData = (): Observable<TablesMap[]> => {
     return this.http.get<TablesMap[]>('http://localhost:3000/table_attributes');
   }
+
+ 
+  sendPostRequest(data: any): Observable<any> {
+    return this.http.post<any>(this.postDataUrl, data);
+}
 }
